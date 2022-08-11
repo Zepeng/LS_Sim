5307a473daed420d43c8a96118d6d27583bad5e6
IHEP <ihep@localhost.localdomain>
2022-08-11 16:06:05 +0800

    move the build directory

--- LS_Sim/build/CMakeFiles/LS_Sim.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/LS_Sim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LS_Sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- LS_Sim/build/CMakeFiles/LS_Sim.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for LS_Sim.
-# This may be replaced when dependencies are built.
